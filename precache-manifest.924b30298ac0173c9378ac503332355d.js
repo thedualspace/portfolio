7f6952735e95172217ab82411459340044e8bd08
thedualspace <64597895+thedualspace@users.noreply.github.com>
2020-11-13 17:51:13 +0000

    Updates

---
+++ precache-manifest.924b30298ac0173c9378ac503332355d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b26fd33b2f4794943c076496a790414",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6964097e38e94466fd70",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b09164ca4c0a17b8fbed",
+    "url": "/portfolio/static/css/main.25b50464.chunk.css"
+  },
+  {
+    "revision": "6964097e38e94466fd70",
+    "url": "/portfolio/static/js/2.f4fa823e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.f4fa823e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09164ca4c0a17b8fbed",
+    "url": "/portfolio/static/js/main.abf293a9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a9b93ed73620b2fdc6753c854c3188be",
+    "url": "/portfolio/static/media/FSLarge.a9b93ed7.png"
+  },
+  {
+    "revision": "40c34b03770a71c6c787f22f325b9011",
+    "url": "/portfolio/static/media/FSSmall.40c34b03.png"
+  },
+  {
+    "revision": "235a94803433f20e9d6521f3375c42e7",
+    "url": "/portfolio/static/media/HyperloopLarge.235a9480.png"
+  },
+  {
+    "revision": "55f8ef9539251cc945a6258b698a9767",
+    "url": "/portfolio/static/media/HyperloopSmall.55f8ef95.png"
+  },
+  {
+    "revision": "adb287102e5703e0ec647dadef29ea4e",
+    "url": "/portfolio/static/media/Jammming1.adb28710.png"
+  },
+  {
+    "revision": "af11aa674b10af5d87c1015e910fb817",
+    "url": "/portfolio/static/media/Jammming2.af11aa67.png"
+  },
+  {
+    "revision": "9ae93ee4e4f9c1a81127e24420312d9f",
+    "url": "/portfolio/static/media/Mandelbrot_wide.9ae93ee4.png"
+  },
+  {
+    "revision": "20f88e2f61ce9bd57ba7cf1f84216670",
+    "url": "/portfolio/static/media/WebLearn1.20f88e2f.png"
+  },
+  {
+    "revision": "5e00ace731be5845a9471d2c91b47b80",
+    "url": "/portfolio/static/media/WebLearn2.5e00ace7.png"
+  },
+  {
+    "revision": "e06dd0c489000068ddd510f1f2bc758c",
+    "url": "/portfolio/static/media/icon-arrow-down.e06dd0c4.svg"
+  },
+  {
+    "revision": "bc8377ae5fee27ee82cae68c79cbb781",
+    "url": "/portfolio/static/media/mandelbrotBloodCell.bc8377ae.png"
+  },
+  {
+    "revision": "3822377103902926cfbe57afa7ff67e8",
+    "url": "/portfolio/static/media/mandelbrotIcicles.38223771.png"
+  },
+  {
+    "revision": "b7d435f71a8bc5368546fb3868366028",
+    "url": "/portfolio/static/media/mandelbrotInferno.b7d435f7.png"
+  },
+  {
+    "revision": "4b52f411695f28992252f57170175a09",
+    "url": "/portfolio/static/media/mandelbrotLightning.4b52f411.png"
+  },
+  {
+    "revision": "ce953658618a6230df33bcb531ca734a",
+    "url": "/portfolio/static/media/mandelbrotNormal.ce953658.png"
+  },
+  {
+    "revision": "edd390f46ac3c6ecde2e57535584967b",
+    "url": "/portfolio/static/media/mandelbrotOctopus.edd390f4.png"
+  },
+  {
+    "revision": "a92d244ed6bb642700f09b03bc9cc617",
+    "url": "/portfolio/static/media/mandelbrotPsychadelic.a92d244e.png"
+  },
+  {
+    "revision": "97cc16e297c3bcbea22d7746282306ad",
+    "url": "/portfolio/static/media/mandelbrotSnowstorm.97cc16e2.png"
+  }
+]);
